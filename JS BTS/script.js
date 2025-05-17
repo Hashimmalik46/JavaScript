0210259cde12e37bea74caac769e0d088202c691
Hashimmalik46 <hashimzahoor2003@gmail.com>
2025-05-17 18:54:54 +0530

    Hoisting and 'this' Keyword

--- JS BTS/script.js
+++ JS BTS/script.js
@@ -38,3 +38,135 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge()*/
+
+
+/*
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Jonas";
+let job = "teacher";
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart(); //This will stil delete the products even though they are 10, because var here is in TDZ = 'undefined' and undefined is a falsy value.
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); //var is present in the global window object which has some implications.
+console.log(y === window.y);
+console.log(z === window.z);
+
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge(); //it will give matilda's age as this keyword points to the object which is calling.
+
+const f = jonas.calcAge;
+f();
+
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that, if we don't do this, the answer would be undefined because in regular fn calls this is undefined.
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      //aarow fns don't get their own 'this' so it will look up in the scope chain and in it's parent scope i.e. calcAge fn this= jonas.
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments); //array of arguments.
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  //arguments keyword is not present in arrow fn.
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);*/
+
+
