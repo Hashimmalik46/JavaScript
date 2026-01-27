6be0d0d5d265408afe06e5db73cba433fff1059a
Hashimmalik46 <hashimzahoor2003@gmail.com>
2026-01-27 19:34:35 +0530

    studied about async await

--- Async-Await/script.js
+++ Async-Await/script.js
@@ -62,7 +62,7 @@ const renderCountry = function (data, className = '') {
         </article>`;
 
   countriesContainer.insertAdjacentHTML('beforeend', countryCard);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 // const getCountryAndNeighbour = function (country) {
@@ -161,100 +161,251 @@ const request = fetch('https://restcountries.com/v2/name/portugal');
 //we can handle errors in 2 ways either using another callback in then
 // .then(response => response.json(), err=> ) or using catch
 
-const getJSON = function (url, errMsg = 'Something went wrong!') {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(`${errMsg}(${response.status})`);
-    }
-    return response.json();
+// const getJSON = function (url, errMsg = 'Something went wrong!') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`${errMsg}(${response.status})`);
+//     }
+//     return response.json();
+//   });
+// };
+
+// const getCountry = function (country) {
+//   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country Not found')
+//     .then(data => {
+//       renderCountry(data[0]);
+
+//       const neigh = data[0].borders?.[0];
+//       if (!neigh) throw new Error('No neighbour found');
+
+//       return getJSON(
+//         `https://restcountries.com/v2/alpha/${neigh}`,
+//         'Neighbour Not Found',
+//       );
+//     })
+//     .then(neighdata => renderCountry(neighdata, 'neighbour'))
+//     .catch(err => {
+//       console.log(`${err}💥💥💥`);
+//       renderError(`Something went wrong! ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', () => {
+//   getCountry('australia');
+// });
+
+// //Event Loop
+// console.log('Test 1'); //1
+// setTimeout(() => {
+//   console.log('0 sec timer'); //2
+// }, 0);
+// Promise.resolve('resolved promise 1').then(res => console.log(res)); //3 promise.resolve() resolves immediately
+// console.log('Test ended'); //4
+// //Seq=1->4->3->2
+// //This happened because microtask queue has priority over callback queue.
+
+// //Creating own promises
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   //Executor function
+//   console.log('Lottery Draw is happening🔮');
+//   //making it async
+//   setTimeout(() => {
+//     if (Math.random() >= 0.5) {
+//       resolve('You Won 💰');
+//     } else {
+//       reject(new Error('You Lost😔'));
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// //Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(1)
+//   .then(() => {
+//     console.log('1 sec Passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('2 secs Passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('3 secs Passed');
+//     return wait(1);
+//   });
+
+// //Callback Hell
+// setTimeout(() => {
+//   console.log('1 sec Passed');
+//   setTimeout(() => {
+//     console.log('2 secs Passed');
+//     setTimeout(() => {
+//       console.log('3 secs Passed');
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem')).catch(err => console.log(err)); //when nothing is to be resolved then we dont use then.
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+// const whereAmI = function (lat, lng) {
+//   fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.`,
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(
+//         `You've entered the wrong coordinates ${response.status}`,
+//       );
+//     }
+//     return response
+//       .json()
+//       .then(location => {
+//         console.log(location);
+//         console.log(`You are in ${location.city}, ${location.countryName}.`);
+//         return fetch(
+//           `https://restcountries.com/v2/name/${location.countryName}`,
+//         );
+//       })
+//       .then(res => {
+//         return res.json().then(country => {
+//           console.log(country[1]);
+//           renderCountry(country[1]);
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err.message);
+//       })
+//       .finally(() => {
+//               countriesContainer.style.opacity = 1;
+//       });
+//   });
+// };
+// whereAmI(52.508, 13.381);
+// // whereAmI(19.037, 72.873);
+// // whereAmI(-33.933, 18.474);
+
+// navigator.geolocation.getCurrentPosition(position => {
+//   (console.log(position), err => console.err(err));
+// });
+// // console.log('Gettting Postion');
+
+// const getPos = function () {
+//   return new Promise(function (resolve, reject) {
+//     // navigator.geolocation.getCurrentPosition(position => {
+//     //   (resolve(position), err => reject(err));
+//     // });
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// const whereAmI = function () {
+//   getPos().then(pos => {
+//     const { latitude: lat, longitude: lng } = pos.coords;
+
+//     fetch(
+//       `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.`,
+//     ).then(response => {
+//       return response.json().then(country => {
+//         console.log(`Your are in ${country.city}.`);
+//       });
+//     });
+//   });
+// };
+// whereAmI();
+
+//Consuming using async/await
+const getPos = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-const getCountry = function (country) {
-  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country Not found')
-    .then(data => {
-      renderCountry(data[0]);
+const whereAmI = async () => {
+  try {
+    const pos = await getPos();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-      const neigh = data[0].borders?.[0];
-      if (!neigh) throw new Error('No neighbour found');
+    const resGeo = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.`,
+    );
+    if (!resGeo.ok) throw new Error('Problem getting data');
 
-      return getJSON(
-        `https://restcountries.com/v2/alpha/${neigh}`,
-        'Neighbour Not Found',
-      );
-    })
-    .then(neighdata => renderCountry(neighdata, 'neighbour'))
-    .catch(err => {
-      console.log(`${err}💥💥💥`);
-      renderError(`Something went wrong! ${err.message}`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+    const dataGeo = await resGeo.json();
+
+    const res = await fetch(
+      `https://restcountries.com/v2/name/${dataGeo.countryName}`,
+    );
+    if (!resGeo.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    renderCountry(data[1]);
+    return `You are in ${dataGeo.city}`;
+  } catch (err) {
+    console.log(`${err}`);
+    renderError(`${err.message}`);
+
+    //Reject Promise from async
+    throw err;
+  }
 };
+whereAmI();
 
-btn.addEventListener('click', () => {
-  getCountry('australia');
-});
+// //Returning values from async
+// console.log('1: Will get Location');
+// // const res = whereAmI();
+// // console.log(res);//showing promise pending
+// whereAmI()
+//   .then(res => console.log(`2: ${res}`))
+//   .catch(err => console.err(`2:${err.message}`))
+//   .finally(() => {
+//     console.log('3: Ending...'); //in order to print 3 after 2 as 2 is async it would be executed at last.
+//   });
+// //if there is an error above it will still return a promise as the promise by async is never rejected.
 
-//Event Loop
-console.log('Test 1'); //1
-setTimeout(() => {
-  console.log('0 sec timer'); //2
-}, 0);
-Promise.resolve('resolved promise 1').then(res => console.log(res)); //3 promise.resolve() resolves immediately
-console.log('Test ended'); //4
-//Seq=1->4->3->2
-//This happened because microtask queue has priority over callback queue.
-
-//Creating own promises
-const lotteryPromise = new Promise(function (resolve, reject) {
-  //Executor function
-  console.log('Lottery Draw is happening🔮');
-  //making it async
-  setTimeout(() => {
-    if (Math.random() >= 0.5) {
-      resolve('You Won 💰');
-    } else {
-      reject(new Error('You Lost😔'));
-    }
-  }, 2000);
-});
-
-lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
-
-//Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
-
-wait(1)
-  .then(() => {
-    console.log('1 sec Passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('2 secs Passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('3 secs Passed');
-    return wait(1);
-  });
-
-//Callback Hell
-setTimeout(() => {
-  console.log('1 sec Passed');
-  setTimeout(() => {
-    console.log('2 secs Passed');
-    setTimeout(() => {
-      console.log('3 secs Passed');
-    }, 1000);
-  }, 1000);
-}, 1000);
-
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Problem')).catch(err => console.log(err)); //when nothing is to be resolved then we dont use then.
-
+//Converting it into aync/await, can be done using IIfs
+(async function () {
+  console.log('1: Will get location!');
+  try {
+    const city = await whereAmI();
+    console.log(city);
+  } catch (err) {
+    console.error(`2:${err.message}`);
+  }
+  console.log('3: Ending...');
+})();
